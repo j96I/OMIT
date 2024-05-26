95e15d03a469828fbb3dab055c979337b59ef587
j96I <j.ibrahim@me.com>
2024-05-26 17:42:09 +1000

    Setup complete

--- src/omit.py
+++ src/omit.py
@@ -1,4 +1,4 @@
-from setup_validation import setup_validate
+from utils.setup_validation import setup_validate
 
 import torch
 
@@ -8,8 +8,13 @@ def fn(x, y):
     b = torch.sin(y).cuda()
     return a + b
 
+
 def omit(): 
-    setup_validate()
+    new_fn = torch.compile(fn, backend="common")
+    input_tensor = torch.randn(10000).to(device="cuda:0")
+    a = new_fn(input_tensor, input_tensor)
+    print(a)
+    
 
 
 if __name__ == '__main__':
