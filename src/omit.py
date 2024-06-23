fcd71d96f888408de662c751d73184c0878ed8ac
j96I <j.ibrahim@me.com>
2024-06-24 01:01:12 +1000

    Use model with result display

--- src/omit.py
+++ src/omit.py
@@ -1,139 +1,57 @@
-from torchvision.transforms import ToTensor, Lambda
-from torch.utils.data import DataLoader, Dataset
-from torchvision import datasets, transforms
-import torchvision.models as models
 import matplotlib.pyplot as plt
-from torch import nn
-import numpy as np
 import torch
-import os
 
-from utils.pytorch_utils import NeuralNetwork, test_loop, train_loop
-from utils.config import *  # noqa: F403
-
-
-# Downloads and displays mnist dataset
-def fashion_mnist_display():
-    labels_map = {
-        0: "T-Shirt",
-        1: "Trouser",
-        2: "Pullover",
-        3: "Dress",
-        4: "Coat",
-        5: "Sandal",
-        6: "Shirt",
-        7: "Sneaker",
-        8: "Bag",
-        9: "Ankle Boot",
-    }
-
-    training_data = datasets.FashionMNIST(
-        root="data", train=True, download=True, transform=ToTensor()
-    )
-    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
-
-    test_data = datasets.FashionMNIST(
-        root="data", train=False, download=True, transform=ToTensor()
-    )
-    test_dataloader = DataLoader(test_data, batch_size=64)
-
-    # Display image and label.
-    train_features, train_labels = next(iter(train_dataloader))
-
-    print(f"Feature batch shape: {train_features.size()}")
-    print(f"Labels batch shape: {train_labels.size()}")
-
-    img = train_features[0].squeeze()
-    label = train_labels[0]
-    print(f"Label: {label}")
-
-    plt.imshow(img, cmap="gray")
-    plt.show()
-
-    # 8x8 image display
-    # figure = plt.figure(figsize=(8, 8))
-    # cols, rows = 3, 3
-    # for i in range(1, cols * rows + 1):
-    #     sample_idx = torch.randint(len(training_data), size=(1,)).item()
-    #     img, label = training_data[sample_idx]
-    #     figure.add_subplot(rows, cols, i)
-    #     plt.title(labels_map[label])
-    #     plt.axis("off")
-    #     plt.imshow(img.squeeze(), cmap="gray")
-    # plt.show()
+from utils.pytorch_training_utils import NeuralNetwork, test_loop, train_loop
+from utils.config import *
 
 
-def fashion_mnist_transform():
-    ds = datasets.FashionMNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=ToTensor(),
-        target_transform=Lambda(
-            lambda y: torch.zeros(10, dtype=torch.float).scatter_(
-                0, torch.tensor(y), value=1
-            )
-        ),
-    )
+def train_model():
+    # Load in premade model
+    # model = torch.load(model_path)
 
-    target_transform = Lambda(
-        lambda y: torch.zeros(10, dtype=torch.float).scatter_(
-            dim=0, index=torch.tensor(y), value=1
-        )
-    )
-
-
-def useNN():
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_available()
-        else "cpu"
-    )
-    print(f"Using {device} device")
-
-    model = NeuralNetwork().to(device)
-    print(model)
-
-    X = torch.rand(1, 28, 28, device=device)
-    logits = model(X)
-    pred_probab = nn.Softmax(dim=1)(logits)
-    y_pred = pred_probab.argmax(1)
-    print(f"Predicted class: {y_pred}")
-
-
-if __name__ == "__main__":
-    training_data = datasets.FashionMNIST(
-        root="data", train=True, download=True, transform=ToTensor()
-    )
-    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
-
-    test_data = datasets.FashionMNIST(
-        root="data", train=False, download=True, transform=ToTensor()
-    )
-    test_dataloader = DataLoader(test_data, batch_size=64)
-
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_available()
-        else "cpu"
-    )
+    # Create model, set to GPU processing
     model = NeuralNetwork().to(device)
 
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  # noqa: F405
+    # Creates a criterion that measures the mean absolute error (MAE)
+    loss_fn = torch.nn.CrossEntropyLoss()
 
-    for t in range(epochs):  # noqa: F405
-        print(f"Epoch {t+1}\n-------------------------------")
+    # Optimizer object which holds the current state
+    # & will update the parameters based on the computed gradients
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    # Per epoch, refine model
+    for t in range(epochs):
+        print(f'Epoch {t+1}\n-------------------------------')
         train_loop(train_dataloader, model, loss_fn, optimizer, device)
         test_loop(test_dataloader, model, loss_fn, device)
 
-    torch.save(model, model_path)  # noqa: F405
+    torch.save(model, model_path)
 
-    # model = torch.load(model_path)  # noqa: F405
     print(model)
+    print('Done!')
 
-    print("Done!")
+
+def use_model(img_index):
+    first_batch = next(iter(test_dataloader))
+    image_tensor, label_index = first_batch[0][img_index], first_batch[1][img_index]
+    img = image_tensor.squeeze()
+
+    model = torch.load(model_path)
+    model.eval()
+
+    with torch.no_grad():
+        image_tensor = image_tensor.to(device)
+        pred = model(image_tensor)
+        predicted, actual = (
+            labels_map[pred[0].argmax(0).item()],
+            labels_map[label_index.item()],
+        )
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+    plt.imshow(img.squeeze(), cmap='gray')
+    plt.title(actual)
+    plt.axis('off')
+    plt.show()
+
+
+use_model(0)
--- src/utils/pytorch_training_utils.py
+++ src/utils/pytorch_training_utils.py
@@ -1,13 +1,7 @@
-from torchvision.transforms import ToTensor, Lambda
-from torch.utils.data import DataLoader, Dataset
-from torchvision import datasets, transforms
-import matplotlib.pyplot as plt
 from torch import nn
-import numpy as np
 import torch
-import os
 
-from utils.config import *  # noqa: F403
+from utils.config import *
 
 
 class NeuralNetwork(nn.Module):
@@ -29,10 +23,11 @@ class NeuralNetwork(nn.Module):
 
 
 def train_loop(dataloader, model, loss_fn, optimizer, device):
-    size = len(dataloader.dataset)
     # Set the model to training mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
     model.train()
+    size = len(dataloader.dataset)
+
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
 
@@ -46,16 +41,16 @@ def train_loop(dataloader, model, loss_fn, optimizer, device):
         optimizer.zero_grad()
 
         if batch % 100 == 0:
-            loss, current = loss.item(), batch * batch_size + len(X)  # noqa: F405
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f'loss: {loss:>7f}  [{current:>5d}/{size:>5d}]')
 
 
 def test_loop(dataloader, model, loss_fn, device):
     # Set the model to evaluation mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
+    model.eval()
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
-    model.eval()
     test_loss, correct = 0, 0
 
     # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
@@ -70,5 +65,5 @@ def test_loop(dataloader, model, loss_fn, device):
     test_loss /= num_batches
     correct /= size
     print(
-        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+        f'Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n'
     )
