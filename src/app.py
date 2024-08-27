e153d8a192e4d19a3fd7d8d2c61067c479cfc954
j96i <j.ibrahim@me.com>
2024-08-27 08:48:46 +0000

    List result

--- src/app.py
+++ src/app.py
@@ -12,8 +12,23 @@ def index():
 def upload_file():
     file = request.files['file']
     
+    # r1 - asyncio.to_thread(requests.get, 'API endpoint')
+
+    # results - await [r1, r2]
+
+    # call api - response in the form of  {tags: ['asdf', 'zxcv]}
+
+    # for each item in response array massage into list - format of <li>data</li>
+
+    # concat outputs - format: <li>tag1</li><li>tag2</li>
+
+    # wrap with ul - format: <ul><li>tag1</li><li>tag2</li></ul>
+
+
     if file:
-        return predict_image(file)
+        data = [predict_image(file), 'item2', 'item3']
+        list_items = ''.join(f'<a>{item}</a>' for item in data)
+        return list_items
 
 if __name__ == '__main__':
     app.run(debug=True)
