e06dd13e3d05500cc2b43e93b124781261357204
j96i <j.ibrahim@me.com>
2024-08-30 03:57:56 +0000

    Web requests +

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from omit import predict_image
+import requests
 
 app = Flask(__name__)
 
@@ -7,10 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/health.html')
+def health():
+    return 'healthy'
+
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
     
+    # fire_url = 'http://10.0.40.136:5000/tags/random'
+    # res = requests.get(fire_url).json()
+    # data.extend(res)
+
     if file:
         tag_list = [predict_image(file), 'item2', 'item3']
         return ''.join(f'<a>{tag}</a>' for tag in tag_list)
